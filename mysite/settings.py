300c0f29d5bd8633d0f8a1c0229e93b4080b5ab7
kensoooooon <kensussu@gmail.com>
2021-10-25 11:38:48 +0900

    change settings.py to allow kensu.pythonanywhere as host.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$dbc&8)4q!y#h-^-=mzvkekmgi-5c&q2h3&kyr55(4gi-(x_e3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'kensu.pythonanywhere.com']
 
 
 # Application definition

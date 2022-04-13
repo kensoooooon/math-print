dc1a3e423665820dec6862d90b717cb626e50e07
kensoooooon <kensussu@gmail.com>
2022-04-13 22:48:30 +0900

    may finish making equation system.

--- math_print/math_process/factorization.py
+++ math_print/math_process/factorization.py
@@ -16,67 +16,113 @@ class FactorizationProblem:
         
         if selected_factorization_type == "ax+ab=a(x+b)":
             latex_answer, latex_problem = self._make_ax_plus_ab_problem()
-        """
         elif selected_factorization_type == "x^2+2ax+a^2=(x+a)^2":
             latex_answer, latex_problem = self._make_square_plus_problem()
         elif selected_factorization_type == "x^2-2ax+a^2=(x-a)^2":
             latex_answer, latex_problem = self._make_square_minus_problem()
         elif selected_factorization_type == "x^2+(a+b)x+ab=(x+a)(x+b)":
             latex_answer, latex_problem = self._make_cross_multiplication_problem()
-        """
         
         return latex_answer, latex_problem
 
     def _make_ax_plus_ab_problem(self):
         number_mode = self._used_coefficient
         x = sy.Symbol("x", real=True) 
-        a = self._make_random_number(8, -8, number_mode)
-        a_latex = sy.latex(a)
-        # b = self._make_random_number(8, -8, "only_integer")
-        b = self._make_random_number(8, -8, "only_integer")
-        b_latex = sy.latex(b)
-        c = self._make_random_number(8, -8, "only_integer")
-        c_latex = sy.latex(c)
-        """
-        problem = a * b * x + a * c
-        answer = sy.factor(problem)
-        print(f"problem: {problem}")
-        print(f"answer: {answer}")
-        
-        latex_answer = sy.latex(answer)
-        latex_problem = sy.latex(problem)
-        """
-        latex_problem = f"{sy.latex(a * b * x + a * c)}"
-        if c > 0:
-            latex_answer = f"= {a_latex} ( {b_latex} x + {c_latex} )"
+        if number_mode == "only_integer":
+            a = self._make_random_number(integer_or_frac_specification="integer")
         else:
-            latex_answer = f"= {a_latex} ( {b_latex} x {c_latex} )"
+            a = self._make_random_number()
+        b = self._make_random_number(integer_or_frac_specification="integer")
+        if number_mode == "only_integer":
+            c = self._make_random_number(integer_or_frac_specification="integer")
+        else:
+            c = self._make_random_number()
+        problem = a * b * x + a * c
+        latex_problem = sy.latex(problem)
+        answer = sy.factor(problem)
+        latex_answer = f"= {sy.latex(answer)}"
         
         return latex_answer, latex_problem
         
-
-    def _make_random_number(self, max_num, min_num, number_mode):
-        # number_mode = self._used_coefficient
-        def make_integer_number():
-            if random() > 0.4:
-                number = randint(2, max_num)
-            else:
-                number = randint(min_num, -1)
-            
-            return number
-        
+    def _make_square_plus_problem(self):
+        # (x+a)^2 => (ax+b)^2 <- after
+        x = sy.Symbol("x", real=True)
+        number_mode = self._used_coefficient
+        # a = self._make_random_number(8, -8, number_mode)
         if number_mode == "only_integer":
-            number = make_integer_number()
-        elif number_mode == "integer_and_frac":
-            if random() > 0.3:
-                number = make_integer_number()
-            else:
-                while True:
-                    numerator = make_integer_number()
-                    denominator = make_integer_number()
-                    number = sy.Rational(numerator, denominator)
-                    
-                    if number.denominator != 1:
-                        break
-        return number
+            b = self._make_random_number(integer_or_frac_specification="integer", positive_or_negative_specification="positive")
+        else:
+            b = self._make_random_number(positive_or_negative_specification="positive")
+        problem = sy.expand((x + b) ** 2)
+        latex_problem = sy.latex(problem)
+        answer = sy.factor(problem)
+        latex_answer = f"= {sy.latex(answer)}"
+        
+        return latex_answer, latex_problem
+
+    def _make_square_minus_problem(self):
+        x = sy.Symbol("x", real=True)
+        number_mode = self._used_coefficient
+        if number_mode == "only_integer":
+            b = self._make_random_number(integer_or_frac_specification="integer", positive_or_negative_specification="negative")
+        else:
+            b = self._make_random_number(positive_or_negative_specification="negative")
+        problem = sy.expand((x + b) ** 2)
+        latex_problem = sy.latex(problem)
+        answer = sy.factor(problem)
+        latex_answer = f"= {sy.latex(answer)}"
+        
+        return latex_answer, latex_problem
     
+    def _make_cross_multiplication_problem(self):
+        x = sy.Symbol("x", real=True)
+        number_mode = self._used_coefficient
+        if number_mode == "only_integer":
+            a = self._make_random_number(integer_or_frac_specification="integer")
+            b = self._make_random_number(integer_or_frac_specification="integer")
+        else:
+            a = self._make_random_number()
+            b = self._make_random_number()
+        
+        problem = sy.expand((x + a) * (x + b))
+        latex_problem = sy.latex(problem)
+        answer = sy.factor(problem)
+        latex_answer = f"= {sy.latex(answer)}"
+        
+        return latex_answer, latex_problem
+
+    def _make_random_number(self, integer_or_frac_specification=None, positive_or_negative_specification=None):
+        
+        def make_random_positive_integer():
+            integer = sy.Integer(randint(2, 8))
+            return integer
+        
+        def make_random_positive_frac():
+            while True:
+                numerator = randint(2, 8)
+                denominator = randint(2, 8)
+                frac = sy.Rational(numerator, denominator)
+                
+                if frac.denominator != 1:
+                    break
+            return frac
+        
+        if integer_or_frac_specification is None:
+            if random() > 0.4:
+                number = make_random_positive_integer()
+            else:
+                number = make_random_positive_frac()
+        elif integer_or_frac_specification == "integer":
+            number = make_random_positive_integer()
+        elif integer_or_frac_specification == "frac":
+            number = make_random_positive_frac()
+        
+        if positive_or_negative_specification is None:
+            if random() > 0.5:
+                number = -1 * number
+        elif positive_or_negative_specification == "negative":
+            number = -1 * number
+        elif positive_or_negative_specification == "positive":
+            pass
+        
+        return number

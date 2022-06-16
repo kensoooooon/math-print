bfcdf0fb8b1ef77eee8947c9da057c5c306f67ee
total-study <inclusivetotalstudy@gmail.com>
2022-06-16 11:43:14 +0900

    need to add four types of check problem.

--- math_print/math_process/sector_with_figure.py
+++ math_print/math_process/sector_with_figure.py
@@ -9,7 +9,7 @@ class SectorWithFigureProblem:
     def __init__(self, **settings):
         sy.init_printing(order='grevlex')
         self._problem_type_list = settings["problem_type_list"]
-        self.latex_answer, self.latex_problem, self.sector = self._make_problem()
+        self.latex_answer, self.latex_problem, self.information_for_shape = self._make_problem()
     
     def _make_problem(self):
         if self._problem_type_list:
@@ -21,47 +21,47 @@ class SectorWithFigureProblem:
             )
         
         if selected_problem_type == "radius_and_central_angle_to_arc_length_and_area":
-            latex_answer, latex_problem, sector = self._make_radius_and_central_angle_to_arc_length_and_area_problem()
+            latex_answer, latex_problem, information_for_shape = self._make_radius_and_central_angle_to_arc_length_and_area_problem()
         elif selected_problem_type == "radius_and_arc_length_to_area":
-            latex_answer, latex_problem, sector = self._make_radius_and_arc_length_to_area_problem()
+            latex_answer, latex_problem, information_for_shape = self._make_radius_and_arc_length_to_area_problem()
         elif selected_problem_type == "radius_and_area_to_arc_length":
-            latex_answer, latex_problem, sector = self._make_radius_and_area_to_arc_length_problem()
+            latex_answer, latex_problem, information_for_shape = self._make_radius_and_area_to_arc_length_problem()
         elif selected_problem_type == "radius_and_area_to_central_angle":
-            latex_answer, latex_problem, sector = self._make_radius_and_area_to_central_angle_problem()
+            latex_answer, latex_problem, information_for_shape = self._make_radius_and_area_to_central_angle_problem()
 
-        return latex_answer, latex_problem, sector
+        return latex_answer, latex_problem, information_for_shape
 
     def _make_radius_and_central_angle_to_arc_length_and_area_problem(self):
-        sector = self._decide_sector_status()
+        sector, information_for_shape = self._decide_sector_status()
         
         latex_problem = f"半径{sector.radius_latex}、中心角{sector.central_angle_latex}のおうぎ形の弧の長さと面積"
         latex_answer = f"弧の長さ: {sector.arc_length_latex}、面積: {sector.area_latex}"
 
-        return latex_answer, latex_problem, sector
+        return latex_answer, latex_problem, information_for_shape
     
     def _make_radius_and_arc_length_to_area_problem(self):
-        sector = self._decide_sector_status()
+        sector, information_for_shape = self._decide_sector_status()
         
         latex_problem = f"半径{sector.radius_latex}、弧の長さ{sector.arc_length_latex}のおうぎ形の面積"
         latex_answer = f"面積: {sector.area_latex}"
         
-        return latex_answer, latex_problem, sector
+        return latex_answer, latex_problem, information_for_shape
     
     def _make_radius_and_area_to_arc_length_problem(self):
-        sector = self._decide_sector_status()
+        sector, information_for_shape = self._decide_sector_status()
         
         latex_problem = f"半径{sector.radius_latex}、面積{sector.area_latex}のおうぎ形の弧の長さ"
         latex_answer = f"弧の長さ: {sector.arc_length_latex}"
         
-        return latex_answer, latex_problem, sector
+        return latex_answer, latex_problem, information_for_shape
     
     def _make_radius_and_area_to_central_angle_problem(self):
-        sector = self._decide_sector_status()
+        sector, information_for_shape = self._decide_sector_status()
         
         latex_problem = f"半径{sector.radius_latex}、面積{sector.area_latex}のおうぎ形の中心角"
         latex_answer = f"中心角{sector.central_angle_latex}"
         
-        return latex_answer, latex_problem, sector
+        return latex_answer, latex_problem, information_for_shape
     
     def _decide_sector_status(self):
         
@@ -70,6 +70,10 @@ class SectorWithFigureProblem:
             arc_length_latex: str
             central_angle_latex: str
             area_latex: str
+        
+        class InformationForShape(NamedTuple):
+            radius: str
+            central_angle: str
         
         radius = randint(1, 10)
         radius_latex = f"\( {sy.latex(radius)} \\mathrm{{ cm }} \)"
@@ -87,7 +91,8 @@ class SectorWithFigureProblem:
         area_latex = f"\( {sy.latex(coefficient_of_area)} \\pi \\mathrm{{ cm^2 }} \)"
         
         sector = Sector(radius_latex, arc_length_latex, central_angle_latex, area_latex)
+        information_for_shape = InformationForShape(str(radius), str(central_angle))
         
-        return sector
+        return sector, information_for_shape
     
         

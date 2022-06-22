c98f6d93ce9d7e8dae3d706343d5396608f674db
kensu <kensussu@gmail.com>
2022-06-22 18:24:03 +0900

    try to make log class.

--- math_print/math_process/logarithm_calculation.py
+++ math_print/math_process/logarithm_calculation.py
@@ -2,6 +2,45 @@ from random import choice, randint
 
 import sympy as sy
 
+"""
+one idea: log making
+
+# self definition of logarithm
+import sympy as sy
+
+class Log:
+    
+    def __init__(self, base_numerator, antilog_numerator, base_denominator=1, antilog_denominator=1):
+        base = sy.Rational(base_numerator, base_denominator)
+        antilog = sy.Rational(antilog_numerator, antilog_denominator)
+        if (base <= 0) or (antilog < 0):
+            raise ValueError(f"base must be more than 0, and antilog must be 0 or more than 0.")
+            
+        self.base = base
+        self.antilog = antilog
+    
+    def __str__(self):
+        return f"log_{self.base} {self.antilog}"
+    
+    def __add__(self, other_log):
+        # same base
+        if self.base == other_log.base:
+            return Log(self.base, self.antilog * other_log.antilog)
+        # different base but can be same base
+    
+    def __sub__(self, other_log):
+        # same base
+        if self.base == other_log.base:
+            return Log(self.base, sy.Rational(self.antilog / other_log.antilog))
+
+num = Log(2, 5)
+print(num)
+num2 = Log(2, 3)
+print(num + num2)
+print(num - num2)
+print(num ** 3)
+"""
+
 class LogarithmCalculationProblem:
     
     def __init__(self, **settings):

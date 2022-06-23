a3a0c4ba3eca2650d957440979db7d2ea82b7eb4
kensu <kensussu@gmail.com>
2022-06-23 17:21:54 +0900

    processing.

--- math_print/math_process/logarithm_calculation.py
+++ math_print/math_process/logarithm_calculation.py
@@ -6,11 +6,12 @@ import sympy as sy
 one idea: log making
 
 # self definition of logarithm
+# self definition of logarithm
 import sympy as sy
 
 class Log:
     
-    def __init__(self, base_numerator, antilog_numerator, base_denominator=1, antilog_denominator=1):
+    def __init__(self, base_numerator, antilog_numerator, base_denominator=1, antilog_denominator=1, coefficient=1, other_part=0):
         base = sy.Rational(base_numerator, base_denominator)
         antilog = sy.Rational(antilog_numerator, antilog_denominator)
         if (base <= 0) or (antilog < 0):
@@ -18,27 +19,60 @@ class Log:
             
         self.base = base
         self.antilog = antilog
+        self.coefficient = coefficient
+        self.other_part = other_part
     
     def __str__(self):
-        return f"log_{self.base} {self.antilog}"
+        log_str = ""
+        if self.coefficient != 0:
+            if self.coefficient == 1:
+                log_str += f"log_{self.base}({self.antilog})"
+            else:
+                log_str += f"{self.coefficient} * log_{self.base} {self.antilog}"
+        if self.other_part != 0:
+            if self.other_part > 0:
+                log_str += f"+{self.other_part}"
+            else:
+                log_str += f"{self.other_part}"
+        
+        return log_str
     
-    def __add__(self, other_log):
+    def __add__(self, other_num):
         # same base
-        if self.base == other_log.base:
-            return Log(self.base, self.antilog * other_log.antilog)
-        # different base but can be same base
+        if isinstance(other_num, Log):
+            if self.base == other_num.base:
+                return Log(self.base, self.antilog * other_num.antilog)
+        else:
+            new_other_part = self.other_part + other_num
+            return Log(self.base, self.antilog, other_part=new_other_part)
     
-    def __sub__(self, other_log):
+    def __sub__(self, other_num):
         # same base
-        if self.base == other_log.base:
-            return Log(self.base, sy.Rational(self.antilog / other_log.antilog))
+        if isinstance(other_num, Log):
+            if self.base == other_num.base:
+                return Log(self.base, self.antilog / other_num.antilog)
+        else:
+            new_other_part = self.other_part - other_num
+            return Log(self.base, self.antilog, other_part=new_other_part)
+            
+    
+    def __mul__(self, other_num):
+        new_coefficient = self.coefficient * other_num
+        return Log(self.base, self.antilog, coefficient=new_coefficient)
+    
+    # def change_of_base_formula(self, new_base):
+        
 
-num = Log(2, 5)
-print(num)
-num2 = Log(2, 3)
-print(num + num2)
-print(num - num2)
-print(num ** 3)
+num1 = Log(2, 5)
+print(f"type: {type(num1)}")
+print(num1)
+print(num1 + 2)
+print(num1 - 3)
+num2 = Log(2, 11)
+print(num1 + num2)
+num3 = Log(2, 2)
+print(num1 + num2 + num3)
+print(3 * num1)
 """
 
 class LogarithmCalculationProblem:

20a35c821ea0aff6bc86252049ba1922f6068a51
kensu <kensussu@gmail.com>
2022-08-26 16:15:29 +0900

    making out_rugby problem.

--- math_print/math_process/elementary5_sector.py
+++ math_print/math_process/elementary5_sector.py
@@ -32,7 +32,7 @@ class Elementary5SectorWithFigureProblem:
         elif selected_problem_type == "in_rugby":
             self.latex_answer, self.one_side_of_square_str = self._make_in_rugby_problem()
         elif selected_problem_type == "out_rugby":
-            pass
+            self.latex_answer, self.one_side_of_square_str = self._make_out_rugby_problem()
     
     def _make_standard_sector_problem(self):
         """標準的な扇形問題の解答と描画に必要な情報を返す
@@ -75,7 +75,7 @@ class Elementary5SectorWithFigureProblem:
         return latex_answer, sector
     
     def _make_in_rugby_problem(self):
-        """ラグビーボール型の面積を求める問題の解答と描画に必要な情報を返す
+        """ラグビーボール型の内側の面積を求める問題の解答と描画に必要な情報を返す
 
         Returns:
             latex_answer (str): latex形式で記述された解答
@@ -83,17 +83,25 @@ class Elementary5SectorWithFigureProblem:
         
         Note:
             値の複雑化を防ぐために、正方形の一辺を5の倍数に限定している。必要があれば複数候補からの選出等に切り替え
-        
-        Developing:
-            .が残っている
         """
         one_side_of_square = randint(1, 10) * 5
         one_side_of_square_str = str(one_side_of_square)
-        sector_area = (one_side_of_square ** 2) * 3.14 *  90 / 360
-        triangle_area = 1 / 2 * (one_side_of_square) ** 2
+        sector_area = (one_side_of_square ** 2) * 3.14 *  sy.Rational(90, 360)
+        triangle_area = sy.Rational(1, 2) * (one_side_of_square) ** 2
         area = 2 * (sector_area - triangle_area)
-        area_str = sy.latex(area).replace("0", "")
-        latex_answer = f"\( {area_str} \\mathrm{{ cm^2 }} \)".replace("\\", "\\\\")
-        print(f"one_side_of_square_str: {one_side_of_square_str}")
+        area_str = sy.latex(area).replace("0", "").rstrip(".")
+        latex_answer = f"\( {sy.latex(area_str)} \\mathrm{{ cm^2 }} \)".replace("\\", "\\\\")
+        return latex_answer, one_side_of_square_str
+    
+    def _make_out_rugby_problem(self):
+        """ラグビーボール型の外側の面積を求める問題の解答と、描画に必要な情報を返す
+        
+        Returns:
+            latex_answer (str): latex形式で記述された解答
+            one_side_of_square_str (str): ラグビーボール型を作る正方形の一辺
+        """
+        one_side_of_square = randint(1, 10) * 5
+        one_side_of_square_str = str(one_side_of_square)
+        latex_answer = ""
         return latex_answer, one_side_of_square_str
     

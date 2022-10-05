fc4c138a903b2ea0baaca3504d8e4ff6384a8c06
kensu <kensussu@gmail.com>
2022-10-05 18:23:23 +0900

    start multiplication and division.

--- math_print/math_process/square_root_calculate.py
+++ math_print/math_process/square_root_calculate.py
@@ -50,66 +50,134 @@ class SquareRootCalculateProblem:
             latex_problem (str): latex形式で記述された問題
         
         Developing:
-            2~4項で計算のタイプが変わる
+            2~5項で計算のタイプが変わる
             2項: 同じ底で足し引き
             3項: 同じ底×2+異なる底×1で計算できるものとできないものの区別
             4項: 同じ底×2+同じ底'×2
             5項: 同じ底×2+同じ底'×2+異なる底
         """
-        prime_numbers = [2, 3, 5, 7, 11, 13]
+        
+        def _latex_formula_maker(coeff_and_base):
+            """与えられた係数と平方根の底から、平方根の表示がランダムで切り替わる和と差の計算式と計算結果を出力
+
+            Args:
+                coeff_and_base (list): 係数と平方根の底のタプルを格納
+
+            Returns:
+                answer_value (sy.Add): 計算結果
+                latex_formula (str): latex形式の計算式
+            
+            Developing:
+                2√3, 2√3みたいなケースが不味そう。(上書き？だったはず？)
+                ->list of tupleのがよき？
+                一部を中に、一部を外にみたいなのは？
+            """
+            answer_value = 0
+            latex_formula = ""
+            for coeff, base in coeff_and_base:
+                answer_value += coeff * sy.sqrt(base)
+                if random() > 0.5:
+                    sqrt_latex = sy.latex(sy.sqrt((coeff ** 2) * base))
+                else:
+                    sqrt_latex = f"\\sqrt{{{(coeff ** 2) * base}}}"
+                if not(latex_formula):
+                    if coeff == 0:
+                        continue
+                    elif coeff > 0:
+                        latex_formula += f"{sqrt_latex}"
+                    else:
+                        latex_formula += f"- {sqrt_latex}"
+                else:
+                    if coeff == 0:
+                        continue
+                    elif coeff > 0:
+                        latex_formula += f"+ {sqrt_latex} "
+                    else:
+                        latex_formula += f"- {sqrt_latex} "
+            return answer_value, latex_formula
+        
+        prime_numbers = [2, 3, 5, 7, 11]
         shuffle(prime_numbers)
         number_of_term = randint(2, 5)
         # two common bases
         coeff_and_base = []
         if number_of_term == 2:
             common_base = prime_numbers.pop()
-            coefficient1 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient1 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient1, common_base))
-            coefficient2 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient2 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient2, common_base))
         # two common bases and one uncommon base 
         elif number_of_term == 3:
             common_base = prime_numbers.pop()
-            coefficient1 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient1 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient1, common_base))
-            coefficient2 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient2 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient2, common_base))
             uncommon_base = prime_numbers.pop()
-            coefficient3 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient3 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient3, uncommon_base))
         # two common bases and two other common bases
         elif number_of_term == 4:
             common_base1 = prime_numbers.pop()
-            coefficient1 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient1 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient1, common_base1))
-            coefficient2 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient2 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient2, common_base1))
             common_base2 = prime_numbers.pop()
-            coefficient3 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient3 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient3, common_base2))
-            coefficient4 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient4 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient4, common_base2))
         # two common bases and two other common bases and one uncommon base
         elif number_of_term == 5:
             common_base1 = prime_numbers.pop()
-            coefficient1 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient1 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient1, common_base1))
-            coefficient2 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient2 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient2, common_base1))
             common_base2 = prime_numbers.pop()
-            coefficient3 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient3 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient3, common_base2))
-            coefficient4 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient4 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient4, common_base2))
             uncommon_base = prime_numbers.pop()
-            coefficient5 = self._make_random_integer(farther_distance_from_zero=3)
+            coefficient5 = self._make_random_integer(farther_distance_from_zero=5)
             coeff_and_base.append((coefficient5, uncommon_base))
         shuffle(coeff_and_base)
-        answer, latex_problem = self._latex_formula_maker(coeff_and_base)
-        latex_answer = sy.latex(answer)
+        answer, latex_problem = _latex_formula_maker(coeff_and_base)
+        latex_answer = f"= {sy.latex(answer)}"
         return latex_answer, latex_problem
     
     def _make_multiplication_and_division_only_problem(self):
+        """積と商のみを用いた計算の問題と解答を出力
+
+        Returns:
+            latex_answer (str): latex形式で記述された解答
+            latex_problem (str): latex形式で記述された問題
+        
+        Developing:
+            2~5項で計算のタイプが変わる
+            2項: 異なる底で掛け割り
+            3項: 同じ底×2+異なる底×1で掛け割り
+            4項: 同じ底×2+同じ底'×2で掛け割り
+            5項: 同じ底×2+同じ底'×2+異なる底で掛け割り
+        """
+        
+        def _latex_formula_maker(coeff_and_base):
+            """与えられた係数の底から、平方根の表示がランダムで切り替わる積と商の計算式と結果を出力
+
+            Args:
+                coeff_and_base (list): 係数と平方根の底と計算方法のタプルを収納
+                
+            Returns:
+                answer_value (sy.Mul): 計算結果
+                latex_formula (str): latex形式の計算式
+            
+            Developing:
+                計算方法: multiplication or division
+            """
+        
         latex_answer = "dummmymmyanswererwewr"
         latex_problem = "dummdududmmmyyyprorororproblem"
         return latex_answer, latex_problem
@@ -118,43 +186,6 @@ class SquareRootCalculateProblem:
         latex_answer = "dummmymmyanswererwewr"
         latex_problem = "dummdududmmmyyyprorororproblem"
         return latex_answer, latex_problem
-    
-    def _latex_formula_maker(self, coeff_and_base):
-        """与えられた係数と平方根の底から、平方根の表示がランダムで切り替わる和と差の計算式と計算結果を出力
-
-        Args:
-            coeff_and_base (list): 係数と平方根の底のタプルを格納
-
-        Returns:
-            answer_value (sy.Add): 計算結果
-            latex_formula (str): latex形式の計算式
-        
-        Developing:
-            2√3, 2√3みたいなケースが不味そう。(上書き？だったはず？)
-            ->list of tupleのがよき？
-        """
-        answer_value = 0
-        latex_formula = ""
-        for coeff, base in coeff_and_base:
-            print(f"coeff: {coeff}, base: {base}")
-            answer_value += coeff * sy.sqrt(base)
-            if random() > 0.5:
-                sqrt_latex = sy.latex(sy.sqrt((coeff ** 2) * base))
-            else:
-                sqrt_latex = f"\\sqrt{{{(coeff ** 2) * base}}}"
-            if not(latex_formula):
-                if coeff == 0:
-                    continue
-                else:
-                    latex_formula += f"{sqrt_latex}"
-            else:
-                if coeff == 0:
-                    continue
-                elif coeff > 0:
-                    latex_formula += f"+ {sqrt_latex} "
-                else:
-                    latex_formula += f"- {sqrt_latex} "
-        return answer_value, latex_formula
 
     def _make_random_integer(self, nearer_distance_from_zero=1, farther_distance_from_zero=10, positive_or_negative=None):
         """原点からの距離がnearer_distance_from_zero以上farther_distance_from_zero以下の範囲の整数を出力

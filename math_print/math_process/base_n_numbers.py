56a526f78573bb6538dac45d5185661cee86dbe3
kensu <kensussu@gmail.com>
2022-10-12 18:24:32 +0900

    processing.

--- math_print/math_process/base_n_numbers.py
+++ math_print/math_process/base_n_numbers.py
@@ -1,3 +1,34 @@
+"""
+from random import randint
+import sympy as sy
+
+base = 5
+digit1 = randint(0, base - 1)
+print(f"digit1: {digit1}")
+digit2 = randint(0, base - 1)
+print(f"digit2: {digit2}")
+integer_part_digits = [digit1, digit2]
+digit3 = randint(0, base - 1)
+print(f"digit3: {digit3}")
+digit4 = randint(0, base - 1)
+print(f"digit4: {digit4}")
+decimal_part_digits = [digit3, digit4]
+
+number_of_digits = len(integer_part_digits)
+index = number_of_digits - 1
+integer_num = 0
+for i, digit in enumerate(integer_part_digits):
+    integer_num += (digit * sy.Pow(base, index - i))
+decimal_num = 0
+index = -1
+for i, digit in enumerate(decimal_part_digits):
+    print(f"index - i: {index - i}")
+    decimal_num += (digit * sy.Pow(base, index - i))
+                            
+print(f"integer_num: {integer_num}")
+print(f"decimal_num: {decimal_num}")
+print(f"sy.Float(decimal_num): {sy.Float(decimal_num)}")
+"""
 from random import choice, randint, random
 
 

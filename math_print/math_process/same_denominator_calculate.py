91128b7c00f85f0908c7973bf16d6e8665d3bc70
kensu <kensussu@gmail.com>
2023-03-29 21:57:44 +0900

    processsing.

--- math_print/math_process/same_denominator_calculate.py
+++ math_print/math_process/same_denominator_calculate.py
@@ -39,8 +39,7 @@ class SameDenominatorCalculate:
         if self._calculate_type == ["addition"]:
             latex_answer, latex_problem = self._make_addition_only_problem()
         elif self._calculate_type == ["subtraction"]:
-            latex_answer = "dummy answer"
-            latex_problem = "dummy problem"
+            latex_answer, latex_problem = self._make_subtraction_only_problem()
         elif self._calculate_type == ["addition", "subtraction"]:
             latex_answer = "dummy answer"
             latex_problem = "dummy problem"
@@ -52,42 +51,13 @@ class SameDenominatorCalculate:
         Returns:
             latex_answer (str): latex形式で記述された解答
             latex_problem (str): latex形式で記述された問題
-        
-        Developing:
-            真分数のみ（帯分数、仮分数はなし）
-            -> 4項は厳しい？
-            -> 分母は最低5以上？
-            -> 分子は最低3以上(1 + 1 + 1)
-            
-        from random import randint, choice
-
-
-        import sympy as sy
-
-
-        # in 3 terms
-
-        for _ in range(20):
-            same_denominator = randint(5, 18)
-            max_numerator = randint(4, same_denominator)
-            print(f"same_denominator: {same_denominator}")
-            print(f"max_numerator: {max_numerator}")
-            numerator1 = randint(1, max_numerator - 3)
-            print(f"numerator1: {numerator1}")
-            # numerator1 + numerator2 < same_denominator
-            # numerator2 < same_denominator - numeartor1
-            numerator2 = randint(1, max_numerator - numerator1 - 2)
-            print(f"numerator2: {numerator2}")
-            numerator3 = randint(1, max_numerator - numerator1 - numerator2 - 1)
-            print(f"numerator3: {numerator3}")
-            print(f"numerator1 + numerator2 + numerator3: {numerator1 + numerator2 + numerator3}")
-            print("-------------------------")
         """
         common_denominator = randint(5, 18)
         term_number = int(choice(self._term_numbers))
         # divided?
         if term_number == 2:
-            numerator1 = randint(1, int(common_denominator / 2))
+            # numerator1 = randint(1, int(common_denominator / 2))
+            numerator1 = randint(1, common_denominator - 1)
             numerator2 = randint(1, common_denominator - numerator1 - 1)
             latex_answer = f"= \\frac{{{numerator1 + numerator2}}}{{{common_denominator}}}"
             if random() > 0.5:
@@ -95,7 +65,51 @@ class SameDenominatorCalculate:
             latex_problem = f"\\frac{{{numerator1}}}{{{common_denominator}}} + \\frac{{{numerator2}}}{{{common_denominator}}}"
             latex_answer = f"= \\frac{{{numerator1 + numerator2}}}{{{common_denominator}}}"
         elif term_number == 3:
-            pass
+            max_numerator = randint(4, common_denominator)
+            numerator1 = randint(1, max_numerator - 3)
+            numerator2 = randint(1, max_numerator - numerator1 - 2)
+            numerator3 = randint(1, max_numerator - numerator1 - numerator2 - 1)
+            numerators = [numerator1, numerator2, numerator3]
+            shuffle(numerators)
+            numerator1, numerator2, numerator3 = numerators
+            latex_problem = f"\\frac{{{numerator1}}}{{{common_denominator}}}"
+            latex_problem += f"+ \\frac{{{numerator2}}}{{{common_denominator}}}"
+            latex_problem += f"+ \\frac{{{numerator3}}}{{{common_denominator}}}"
+            latex_answer = f"= \\frac{{{numerator1 + numerator2 + numerator3}}}{{{common_denominator}}}"
         else:
             raise ValueError(f"'term_number' is {term_number}. This must be 2 or 3.")
         return latex_answer, latex_problem
+    
+    def _make_subtraction_only_problem(self):
+        """引き算だけの計算問題を作成
+        
+        Returns:
+            latex_answer (str): latex形式で記述された解答
+            latex_problem (str): latex形式で記述された問題
+        
+        Developing:
+            ・真分数
+            ・結果は正
+        """
+        common_denominator = randint(6, 20)
+        term_number = int(choice(self._term_numbers))
+        if term_number == 2:
+            numerator1 = randint(2, common_denominator - 1)
+            numerator2 = randint(1, numerator1 - 1)
+            latex_problem = f"\\frac{{{numerator1}}}{{{common_denominator}}} - \\frac{{{numerator2}}}{{{common_denominator}}}"
+            latex_answer = f"= \\frac{{{numerator1 - numerator2}}}{{{common_denominator}}}"
+        elif term_number == 3:
+            numerator1 = randint(4, common_denominator - 1)
+            sum_of_numerator2_and_3 = randint(2, numerator1 - 1)
+            numerator2 = randint(1, sum_of_numerator2_and_3 - 1)
+            numerator3 = sum_of_numerator2_and_3 - numerator2
+            if random() > 0.5:
+                numerator2, numerator3 = numerator3, numerator2
+            latex_problem = f"\\frac{{{numerator1}}}{{{common_denominator}}}"
+            latex_problem += f"- \\frac{{{numerator2}}}{{{common_denominator}}}"
+            latex_problem += f"- \\frac{{{numerator3}}}{{{common_denominator}}}"
+            latex_answer = f"= \\frac{{{numerator1 - numerator2 - numerator3}}}{{{common_denominator}}}"
+        else:
+            raise ValueError(f"'term_number' is {term_number}. This must be 2 or 3.")
+        return latex_answer, latex_problem
+        

07deb685650b413e4212b57682ff3f684cb737cb
total-study <inclusivetotalstudy@gmail.com>
2023-04-17 12:48:54 +0900

    processing.

--- math_print/math_process/line_and_flat_positional_relationship.py
+++ math_print/math_process/line_and_flat_positional_relationship.py
@@ -17,29 +17,55 @@ class LineAndFlatPositionalRelationship:
         Args:
             settings (dict): 問題に使用する各種設定を格納
         """
+        problem_types = settings["used_problems"]
         selected_solid_body = choice(settings["used_solid_bodies"])
         if selected_solid_body == "quadrangular_prism":
             self.solid_body = "quadrangular_prism"
-            self.latex_answer, self.latex_problem = self._make_quadrangular_prism_problem()
+            self.latex_answer, self.latex_problem = self._make_quadrangular_prism_problem(problem_types)
         elif selected_solid_body == "triangular_prism":
             self.solid_body = "triangular_prism"
-            self.latex_answer, self.latex_problem = self._make_triangular_prism_problem()
+            self.latex_answer, self.latex_problem = self._make_triangular_prism_problem(problem_types)
         else:
             raise ValueError(f"'selected_solid_body' is {selected_solid_body}. This must be 'quadrangular_prism' or 'triangular_prism.")
     
-    def _make_quadrangular_prism_problem(self):
+    def _make_quadrangular_prism_problem(self, problem_types):
         """直方体を用いた直線と平面の位置関係の問題を作成
+
+        Args:
+            problem_types (list): 使用される問題(直線と直線, 直線と平面, 平面と平面)が格納されている
 
         Returns:
             latex_answer (str): latex形式で記述された解答
             latex_problem (str): latex形式で記述された問題
         
         Developing:
-            上面から反時計回りでABCD, EFGHの直方体
+            上面左手前から反時計回りでABCD, EFGHの直方体
+            答えさせる問題のタイプ
+            -> ある辺に平行な直線は？
+            -> ある辺と垂直な直線は？
+            -->> 直線同士の位置関係
+            -->> 平面同士の位置関係
+            -->> 直線と平面の位置関係
         """
-        
-        latex_answer = "dummy answer in quadrangular problem."
-        latex_problem = "dummy problem in quadrangular problem."
+        latex_answer = ""
+        latex_problem = ""
+        for _ in range(3):
+            selected_problem_type = choice(problem_types)
+            if selected_problem_type == "line_and_line":
+                """
+                二直線の位置関係
+                -> 平行, ねじれ, 垂直に交わる, (垂直以外に交わる)
+                """
+            elif selected_problem_type == "line_and_flat":
+            elif selected_problem_type == "flat_and_flat":
+            else:
+                raise ValueError(f"'selected_problem_type' is {selected_problem_type}."\
+                                 "This must be 'line_and_line', 'line_and_flat' or 'flat_and_flat'.")
+        parallel_lines = (
+            ("直線AB", "直線CD", "直線EF", "直線GH",),
+            ("直線AD", "直線BC", "直線EH", "直線FG",),
+            ("直線AE", "直線BF", "直線CG", "直線DH",),
+            )
         return latex_answer, latex_problem
     
     def _make_triangular_prism_problem(self):
--- math_print/views.py
+++ math_print/views.py
@@ -2662,14 +2662,28 @@ def display_same_denominator_calculate_problem(request):
 
 
 def display_line_and_flat_positional_relationship(request):
+    """直線と平面の位置関係の問題のリクエスト受信と問題出力を担当
+    
+    Args:
+        request (django.core.handlers.wsgi.WSGIRequest): 送信されたリクエスト
+    
+    Returns:
+        math_problem_tuple_list (list): 2問ずつ問題が格納されたタプルのリスト
+    """
+    print(f"type: {type(request)}")
     PROBLEM_NUMBER = 10
+    used_problems = request.POST.getlist("used_problem")
+    if not(used_problems):
+        used_problems.append("line_and_line")
+        used_problems.append("line_and_flat")
+        used_problems.append("flat_and_flat")
     used_solid_bodies = request.POST.getlist("used_solid_body")
     if not(used_solid_bodies):
         used_solid_bodies.append("quadrangular_prism")
         used_solid_bodies.append("triangular_prism")
     math_problem_tuple_list = []
     for _ in range(int(PROBLEM_NUMBER // 2)):
-        problem1 = LineAndFlatPositionalRelationship(used_solid_bodies=used_solid_bodies)
-        problem2 = LineAndFlatPositionalRelationship(used_solid_bodies=used_solid_bodies)
+        problem1 = LineAndFlatPositionalRelationship(used_problems=used_problems, used_solid_bodies=used_solid_bodies)
+        problem2 = LineAndFlatPositionalRelationship(used_problems=used_problems, used_solid_bodies=used_solid_bodies)
         math_problem_tuple_list.append((problem1, problem2))
     return render(request, 'math_print/junior_highschool1/line_and_flat_positional_relationship/for_display.html', {'math_problem_tuple_list': math_problem_tuple_list})

5d529ed5434df3645b40357694fde8271d3d12bf
kensoooooon <kensussu@gmail.com>
2023-06-23 11:10:39 +0900

    processing.

--- math_print/math_process/logarithmic_equation.py
+++ math_print/math_process/logarithmic_equation.py
@@ -1,5 +1,5 @@
 from random import choice, random, randint
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple, Union
 
 
 import sympy as sy
@@ -40,16 +40,31 @@ class LogarithmicEquation:
             latex_problem (str): latex形式と通常の文字が混在した問題
         
         Developing:
-            log(a)(bx + c) = log(a)(dx + e)
             log(a)(bx + c) = d
-            log(a)(bx + c)(dx + e) = log(a)(fx + g)(hx + i)
+            log(a)(bx + c)(dx + e) = f
         """
-        selected_equation_type = choice(["log(a)(bx + c) = log(a)(dx + e)", "log(a)(bx + c) = d", "log(a)(bx + c)(dx + e) = log(a)(fx + g)(hx + i)"])
-        if selected_equation_type == "log(a)(bx + c) = log(a)(dx + e)":
-            pass
-        elif selected_equation_type == "log(a)(bx + c) = d":
-            pass
-        elif selected_equation_type == "log(a)(bx + c)(dx + e) = log(a)(fx + g)(hx + i)":
+        x = sy.Symbol("x", real=True)
+        selected_equation_type = choice(["log(a)(bx + c) = d", "log(a)(bx + c)(dx + e) = f"])
+        if selected_equation_type == "log(a)(bx + c) = d":
+            """
+            log(a)(bx + c) = log(a)(a^d)
+            bx + c = a^d
+            bx = a^d - c
+            x = (a^d - c) / b
+                bx + c > 0
+                    x > -c/b(b > 0)
+                    x < -c/b(b < 0)
+            a > 0
+            b > 0
+            c: any?
+            d: int
+            """
+            a = randint(2, 4)
+            b = randint(1, 6)
+            c = randint(0, 6)
+            d = randint(1, 3)
+            
+        elif selected_equation_type == "log(a)(bx + c)(dx + e) = f":
             pass
         latex_answer = "dummy answer in _make_only_with_calculation_problem"
         latex_problem = "dummy problem in _make_only_with_calculation_problem"
@@ -76,3 +91,43 @@ class LogarithmicEquation:
         latex_answer = "dummy answer in _make_with_replacement_problem"
         latex_problem = "dummy problem in _make_with_replacement_problem"
         return latex_answer, latex_problem
+    
+    def _random_number(self, integer_or_frac=None, positive_or_negative=None, remove_zero=None, remove_one=None):
+        # not one == not zero, frac, 
+
+        if positive_or_negative is None:
+            pass
+        elif positive_or_negative == "positive":
+            number = sy.Abs(number)
+        elif positive_or_negative == "negative":
+            number = -1 * sy.Abs(number)
+        return number
+    
+    def _random_integer(self, positive_or_negative: Optional[str] = None, remove_zero: Optional[bool] = None, remove_one: Optional[bool]=None) -> sy.Integer:
+        """指定された条件の整数を出力し、底や真数などに利用する
+        
+        Args:
+            positive_or_negative (:obj:`str`, optional): 正負の指定。 Defaults to None.
+            remove_zero (:obj:`bool`, optional): 1を除くかどうかの指定。 Defaults to None.
+            remove_one (:obj:`bool`, optional): 1を除くかどうかの指定。 Defaults to None.
+        """
+        if remove_one is None:
+            if remove_zero is None:
+                integer = sy.Integer(randint(0, 6))
+            else:
+                integer = sy.Integer(randint(1, 6))
+        else:
+            if remove_zero is None:
+                integer = sy.Integer(choice[0, 2, 3, 4, 5, 6])
+            else:
+                integer = sy.Integer(randint(2, 6))
+        
+        if positive_or_negative is None:
+            pass
+        elif positive_or_negative == "positive":
+            integer = sy.Abs(integer)
+        elif positive_or_negative == "negative":
+            integer = -1 * sy.Abs(integer)
+        return integer
+        
+    def _random_frac(self, positive_or_negative=None, remove_zero=None):

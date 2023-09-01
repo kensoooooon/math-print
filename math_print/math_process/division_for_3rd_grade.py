535aad71a432feac9d239a772148e56e32fb5089
kensoooooon <kensussu@gmail.com>
2023-09-01 12:08:58 +0900

    processing.

--- math_print/math_process/division_for_3rd_grade.py
+++ math_print/math_process/division_for_3rd_grade.py
@@ -15,10 +15,14 @@ class DivisionFor3rdGrade:
     def __init__(self, **settings: Dict) -> None:
         sy.init_printing(order="grevlex")
         selected_remainder_type = choice(settings["remainder_types"])
+        if selected_remainder_type == "with_remainder":
+            with_remainder = True
+        elif selected_remainder_type == "without_remainder":
+            with_remainder = False
         selected_digit_of_divided_number = int(choice(settings["digit_of_divided_numbers"]))
-        self.latex_answer, self.latex_problem = self._make_problem(selected_remainder_type, selected_digit_of_divided_number)
+        self.latex_answer, self.latex_problem = self._make_problem(with_remainder, selected_digit_of_divided_number)
     
-    def _make_problem(self, selected_remainder_type: str, selected_digit_of_divided_number: int) -> Tuple[str, str]:
+    def _make_problem(self, with_remainder: bool, selected_digit_of_divided_number: int) -> Tuple[str, str]:
         """指定された余りの有無と、割られる数の桁数に応じた問題と解答を出力する
         
         Args:
@@ -29,18 +33,36 @@ class DivisionFor3rdGrade:
             latex_answer (str): latex形式で表記された割り算の答え
             latex_problem (str): latex形式で表示された割り算の問題の式
         """
-        if selected_digit_of_divided_number == 1:
-            divided_number = randint(0, 9)
-        elif selected_digit_of_divided_number == 2:
-            divided_number = randint(10, 99)
-        dividing_number = randint(1, 9)
-        quotient, remainder = divmod(divided_number, dividing_number)
-        if selected_remainder_type == "without_remainder":
-            divided_number = divided_number - remainder
+        if with_remainder:
+            if selected_digit_of_divided_number == 1:
+                divided_number = randint(1, 9)
+            elif selected_digit_of_divided_number == 2:
+                divided_number = randint(10, 99)
+            one_digit_divisors = [divisor for divisor in sy.divisors(divided_number) if divisor < 10]
+            dividing_numbers = [dividing_number for dividing_number in range(1, 10) if dividing_number not in one_digit_divisors]
+            dividing_number = choice(dividing_numbers)
             quotient, remainder = divmod(divided_number, dividing_number)
-            latex_answer = f"\\( {sy.latex(quotient)} \\)"
-        elif selected_remainder_type == "with_remainder":
-            latex_answer = f"\\( {sy.latex(quotient)} \\) あまり \\( {sy.latex(remainder)} \\)"
-        latex_problem = f"\\( {sy.latex(divided_number)} \\div {sy.latex(dividing_number)} \\)"
+            if remainder == 0:
+                raise ValueError(f"remainder is {remainder}. It must not be 0.")
+            latex_answer = f"\\( {quotient} \\) あまり\\( {remainder} \\)"
+        else:
+            if selected_digit_of_divided_number == 1:
+                divided_number = randint(0, 9)
+            elif selected_digit_of_divided_number == 2:
+                divided_number = randint(10, 99)
+            if divided_number == 0:
+                dividing_number = randint(1, 9)
+            else:
+                one_digit_divisors = [divisor for divisor in sy.divisors(divided_number) if divisor < 10]
+                if one_digit_divisors == [1]:
+                    dividing_number = 1
+                else:
+                    one_digit_divisors.remove(1)
+                    dividing_number = choice(one_digit_divisors)
+            quotient, remainder = divmod(divided_number, dividing_number)
+            if remainder != 0:
+                raise ValueError(f"remainder is {remainder}. It must be 0.")
+            latex_answer = f"\\( {quotient} \\)"
+        latex_problem = f"\\( {divided_number} \\div {dividing_number} \\)"
         return latex_answer, latex_problem
     

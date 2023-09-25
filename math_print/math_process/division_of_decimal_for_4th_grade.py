22587bba7905b16643331737ee22a268df784322
kensoooooon <kensussu@gmail.com>
2023-09-25 12:06:12 +0900

    processing.

--- math_print/math_process/division_of_decimal_for_4th_grade.py
+++ math_print/math_process/division_of_decimal_for_4th_grade.py
@@ -1,145 +1,31 @@
 """
-from random import randint
 import sympy as sy
 
 def truncate_decimal(number, decimal_places):
     return sy.floor(number * 10 ** decimal_places) / 10 ** decimal_places
 
-def random_decimal(number_of_decimal_places):
-    integer_part = randint(1, 50)
-    if number_of_decimal_places == 0:
-        number = sy.Integer(integer_part)
-    elif number_of_decimal_places == 1:
-        decimal_part = randint(1, 9) * sy.Float(0.1)
-        number = integer_part + decimal_part
-    elif number_of_decimal_places == 2:
-        decimal_part = randint(0, 9) * sy.Float(0.1) + randint(1, 9) * sy.Float(0.01)
-        number = integer_part + decimal_part
-    elif number_of_decimal_places == 3:
-        decimal_part = randint(0, 9) * sy.Float(0.1) + randint(0, 9) * sy.Float(0.01) + randint(1, 9) * sy.Float(0.001)
-        number = integer_part + decimal_part
-    return number
-
-for _ in range(3):
-    dividing_number_of_decimal_places = randint(0, 3)
-    print(f"dividing_number_of_decimal_places: {dividing_number_of_decimal_places}")
-    dividing_number = random_decimal(dividing_number_of_decimal_places)
-    print(f"dividing_number: {dividing_number}")
-    print(f"dividing_latex_number: {sy.latex(dividing_number)}")
-    print("-----------")
-    divided_number_of_decimal_places = randint(0, 3)
-    print(f"divided_number_of_decimal_places: {divided_number_of_decimal_places}")
-    divided_number = random_decimal(divided_number_of_decimal_places)
-    print(f"divided_number: {divided_number}")
-    print(f"divided_latex_number: {sy.latex(divided_number)}")
-    print("-----------")
-    answer = divided_number / dividing_number
-    print(f"answer: {answer}")
-    truncated_answer = truncate_decimal(answer, 2)
-    print(f"truncated_answer: {truncated_answer}")
-    floated_truncated_answer = sy.Float(truncated_answer)
-    print(f"floated_truncated_answer: {floated_truncated_answer}")
-    floated_truncated_latex_answer = sy.latex(floated_truncated_answer)
-    print(f"floated_truncated_latex_answer: {floated_truncated_latex_answer}")
-    print("-----------")
-    # a ÷ b = q ... r
-    # a = bq + r
-    # r = a - bq
-    remainder = divided_number - dividing_number * floated_truncated_answer
-    print(f"remainder: {remainder}")
-    print(f"latex_remainder: {sy.latex(remainder)}")
-    print("----------------------------------")
-    
-
-# conversion
-from random import randint, uniform
-import sympy as sy
-
-
-def truncate_decimal(number, decimal_places):
-    return sy.floor(number * 10 ** decimal_places) / 10 ** decimal_places
-
-def random_decimal(number_of_decimal_places):
-    integer_part = randint(1, 50)
-    if number_of_decimal_places == 0:
-        number = sy.Integer(integer_part)
-    elif number_of_decimal_places == 1:
-        decimal_part = randint(1, 9) * sy.Float(0.1)
-        number = integer_part + decimal_part
-    elif number_of_decimal_places == 2:
-        decimal_part = randint(0, 9) * sy.Float(0.1) + randint(1, 9) * sy.Float(0.01)
-        number = integer_part + decimal_part
-    elif number_of_decimal_places == 3:
-        decimal_part = randint(0, 9) * sy.Float(0.1) + randint(0, 9) * sy.Float(0.01) + randint(1, 9) * sy.Float(0.001)
-        number = integer_part + decimal_part
-    return number
-
-
-# a = bq + r <=> a ÷ b = q ... r
-b = random_decimal(0)
-print(f"b: {b}")
-q = random_decimal(1)
-print(f"q: {q}")
-r = uniform(0, 0.1 * q)
-print(f"r: {r}")
-truncated_r = truncate_decimal(r, 1)
-print(f"truncated_r: {truncated_r}")
-
-
-
-import sympy as sy
-
-# divided_number = sy.N(sy.Float(9.3), 2)
-divided_number = sy.Float(9.3)
+# divided_number = sy.Float(51.8)
+divided_number = sy.Rational(578, 10)
 print(f"divided_number: {divided_number}")
-dividing_number = sy.Integer(38)
+print(f"latex_divided_number: {sy.latex(divided_number)}")
+print(f"floated_latex_divided_number: {sy.latex(sy.Float(divided_number))}")
+dividing_number = sy.Rational(12, 10)
 print(f"dividing_number: {dividing_number}")
-answer = (divided_number / dividing_number).round(2)
-print(f"answer: {answer}")
-remainder = divided_number - dividing_number * answer
-print(f"remainder: {remainder}")
-print("------------------------")
-
-divided_number = sy.Float(9.3)
-print(f"divided_number: {divided_number}")
-dividing_number = sy.Integer(38)
-print(f"dividing_number: {dividing_number}")
+print(f"floated_latex_dividing_number: {sy.latex(sy.Float(divided_number))}")
+print(f"latex_dividing_number: {sy.latex(dividing_number)}")
+print("--------")
 answer = divided_number / dividing_number
 print(f"answer: {answer}")
-rounded1_answer = answer.round(1)
-print(f"rounded1_answer: {rounded1_answer}")
-rounded2_answer = answer.round(2)
-print(f"rounded2_answer: {rounded2_answer}")
-rounded3_answer = answer.round(3)
-print(f"rounded3_answer: {rounded3_answer}")
-rounded4_answer = answer.round(4)
-print(f"rounded4_answer: {rounded4_answer}")
-remainder_with_answer = divided_number - dividing_number * answer
-print(f"remainder_with_answer: {remainder_with_answer}")
-remainder_with_rounded1_answer = divided_number - dividing_number * rounded1_answer
-print(f"remainder_with_rounded1_answer: {remainder_with_rounded1_answer}")
-remainder_with_rounded2_answer = divided_number - dividing_number * rounded2_answer
-print(f"remainder_with_rounded2_answer: {remainder_with_rounded2_answer}")
-remainder_with_rounded3_answer = divided_number - dividing_number * rounded3_answer
-print(f"remainder_with_rounded3_answer: {remainder_with_rounded3_answer}")
-remainder_with_rounded4_answer = divided_number - dividing_number * rounded4_answer
-print(f"remainder_with_rounded4_answer: {remainder_with_rounded4_answer}")
-print("------------------------")
-
-import sympy as sy
-
-num1 = sy.N(sy.Float(58.1), 4)
-print(f"num1: {num1}")
-print(f"latex_num1: {sy.latex(num1)}")
-num2 = sy.Integer(27)
-print(f"num2: {num2}")
-print(f"latex_num2: {sy.latex(num2)}")
-print("--------")
-answer = num1 / num2
-print(f"answer: {answer}")
-print(f"num2 * answer: {num2 * answer}")
-remainder = num1 - num2 * answer
+print(f"dividing_number * answer: {dividing_number * answer}")
+# truncated_answer = sy.N(answer, 4)
+truncated_answer = truncate_decimal(answer, 0)
+rounded_answer = answer.round(2)
+print(f"truncated_answer: {truncated_answer}")
+print(f"rounded_answer: {rounded_answer}")
+# print(f"floated_latex_truncated_answer: {sy.latex(sy.Float(truncated_answer))}")
+remainder = divided_number - dividing_number * truncated_answer
 print(f"remainder: {remainder}")
+print(f"floated_latex_remainder: {sy.latex(sy.Float(remainder))}")
 print("--------------------")
 """
 from random import randint, choice
@@ -186,41 +72,55 @@ class DivisionOfDecimalFor4thGrade:
         Returns:
             tuple: 解答と問題(latex_answer, latex_problem)
         """ 
-        divided_number, divided_number_latex = self._random_number(divided_number_of_decimal_places)
-        dividing_number, dividing_number_latex = self._random_number(dividing_number_of_decimal_places)
-        latex_problem = f"\\( {divided_number_latex} \\div {dividing_number_latex} \\)"
-        answer = divided_number / dividing_number
-        # truncated_answer, truncated_answer_latex = self._truncate_decimal(answer, 2)
-        rounded_answer = sy.N(answer, 3)
-        print(f"rounded_answer: {rounded_answer}, type: {type(rounded_answer)}")
-        rounded_answer_latex = sy.latex(rounded_answer)
-        remainder = divided_number - rounded_answer * dividing_number
-        # truncated_remainder, truncated_remainder_latex = self._truncate_decimal(remainder, 3)
-        rounded_remainder = sy.N(remainder, 3)
-        print(f"rounded_remainder: {rounded_remainder}, type: {type(rounded_remainder)}")
-        print("----------------------------")
-        rounded_remainder_latex = sy.latex(rounded_remainder)
-        latex_answer = f"\\( {rounded_answer_latex} \\) あまり \\( {rounded_remainder_latex} \\)"
+        if with_remainder:
+            divided_number, divided_number_latex = self._random_number(divided_number_of_decimal_places)
+            print(f"divided_number: {divided_number}, divided_number_latex: {divided_number_latex}")
+            dividing_number, dividing_number_latex = self._random_number(dividing_number_of_decimal_places)
+            print(f"dividing_number: {dividing_number}, dividing_number_latex: {dividing_number_latex}")
+            latex_problem = "次の割り算の商を小数第二位まで求めましょう。"
+            latex_problem += "\n また、あまりがあるときは、それもだしなさい。"
+            latex_problem += f"\n \\( {divided_number_latex} \\div {dividing_number_latex} \\)"
+            answer = divided_number / dividing_number
+            print(f"answer: {answer}, latex_floated_answer: {sy.latex(sy.Float(answer))}")
+            truncated_answer, truncated_answer_latex = self._truncate_decimal(answer, 2)
+            print(f"truncated_answer: {truncated_answer}, truncated_answer_latex: {truncated_answer_latex}")
+            remainder = divided_number - dividing_number * truncated_answer
+            remainder_latex = sy.latex(sy.Float(remainder))
+            print(f"remainder: {remainder}, remainder_latex: {remainder_latex}")
+            if remainder == 0:
+                latex_answer = f"\\( {truncated_answer_latex} \\)"
+            else:
+                latex_answer = f"\\( {truncated_answer_latex} \\) あまり \\( {remainder_latex} \\)"
+            print(f"round_answer: {answer.round(4)}")
+            print("-------------------------")
+        else:
+            latex_problem = "dummy problem without remainder"
+            latex_answer = "dummy answer without remainder"
         return latex_answer, latex_problem
-
-    def _random_number(self, number_of_decimal_places: int) -> Tuple[Union[sy.Integer, sy.Float], str]:
-        """指定された小数点以下の桁数が保証された小数(整数含む)を出力
+    
+    def _random_number(self, number_of_decimal_places: int) -> Tuple[Union[sy.Integer, sy.Rational], str]:
+        """ランダムに生成された、指定された条件を満たす整数や小数、および出力用の文字を合わせて返す
 
         Args:
-            number_of_decimal_places (int): 小数点以下の桁数。0のときは整数
+            number_of_decimal_places (int): 条件として指定される小数点以下の桁数。0のときは小数部は存在せず、整数が返される
 
         Returns:
-            Union[sy.Integer, sy.Float]: 整数のときはsy.Integer, 小数のときはsy.Floatをそれぞれ返す
+            Tuple[Union[sy.Integer, sy.Rational], str]:
+                この関数は2つの値を返します。
+                - number (Union[sy.Integer, sy.Rational]): 実際の計算に用いる数。問題の設定次第で表示すべき桁数が細かく変わるため、評価が後で行えるように小数型への変換は行わない
+                - latex_number (str): latex形式で記述された数
         """
+        integer_part = sy.Integer(randint(1, 50))
         if number_of_decimal_places == 0:
-            number = sy.Integer(randint(1, 20))
-            number_latex = sy.latex(number)
+            number = integer_part
+            latex_number = sy.latex(number)
         else:
             numerator = randint(10 ** (number_of_decimal_places - 1), 10 ** number_of_decimal_places - 1)
             denominator = 10 ** number_of_decimal_places
-            number = sy.Rational(numerator, denominator)
-            number_latex = sy.latex(sy.Float(number))
-        return number, number_latex
+            decimal_part = sy.Rational(numerator, denominator)
+            number = integer_part + decimal_part
+            latex_number = sy.latex(sy.Float(number))
+        return number, latex_number
 
     def _truncate_decimal(self, number: sy.Rational, number_of_decimal_places: int) -> Tuple[sy.Rational, str]:
         """指定された桁数より下を切り捨てた小数の文字列を出力
@@ -237,5 +137,19 @@ class DivisionOfDecimalFor4thGrade:
         str_number = sy.latex(sy.Float(floored_number))
         return floored_number, str_number
     
-    def _round_decimal(self, number, number_of_decimal_places):
-        pass
+    def _round_decimal(self, number: Union[sy.Integer, sy.Rational], number_of_decimal_places: int) -> Tuple[Union[sy.Integer, sy.Rational], str]:
+        """指定された桁数より下を四捨五入して丸めた小数を出力
+
+        Args:
+            number (Union[sy.Integer, sy.Rational]): 桁数を四捨五入したい数
+            number_of_decimal_places (int): 残したい桁数
+
+        Returns:
+            Tuple[Union[sy.Integer, sy.Rational], str]:
+                この関数は、以下の2つの値を返します
+                - rounded_number (Union[sy.Integer, sy.Rational]): 四捨五入して丸められた実際の数
+                - latex_rounded_number (str): latex形式として表示された数
+        """
+        rounded_number = number.round(number_of_decimal_places)
+        latex_rounded_number = sy.latex(sy.Float(rounded_number))
+        return rounded_number, latex_rounded_number

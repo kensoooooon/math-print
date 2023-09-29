c24ab07a20f178d3c5e43fef43e526912a2e44f7
kensoooooon <kensussu@gmail.com>
2023-09-29 11:52:36 +0900

    finished making addition. next is subtraction and fill in the square.

--- math_print/math_process/addition_and_subtraction_of_fraction_for_4th_grade.py
+++ math_print/math_process/addition_and_subtraction_of_fraction_for_4th_grade.py
@@ -1,3 +1,7 @@
+"""
+約分なし。
+gcd=1で通るが、計算するともとに戻る
+"""
 from random import choice, randint
 from typing import Dict, Tuple, Union
 
@@ -26,7 +30,11 @@ class AdditionAndSubtractionOfFraction:
             with_integer_part = True
         elif selected_integer_part == "without_integer_part":
             with_integer_part = False
-        if selected_calculation == "addition":
+        if selected_calculation == "from_improper_fraction_to_mixed_number_or_integer":
+            self.latex_answer, self.latex_problem = self._make_from_improper_fraction_to_mixed_number_or_integer_problem()
+        elif selected_calculation == "from_mixed_number_to_improper_fraction":
+            self.latex_answer, self.latex_problem = self._make_from_mixed_number_to_improper_fraction_problem()
+        elif selected_calculation == "addition":
             self.latex_answer, self.latex_problem = self._make_addition_problem(with_integer_part)
         elif selected_calculation == "subtraction":
             self.latex_answer, self.latex_problem = self._make_subtraction_problem(with_integer_part)
@@ -34,6 +42,40 @@ class AdditionAndSubtractionOfFraction:
             self.latex_answer, self.latex_problem = self._make_fill_in_the_square_problem(with_integer_part)
         else:
             raise ValueError(f"'selected_calculation' is {selected_calculation}. It must be 'addition', 'subtraction' or 'fill_in_the_square'.")
+    
+    def _make_from_improper_fraction_to_mixed_number_or_integer_problem(self):
+        """仮分数から帯分数への変換
+
+        Returns:
+            Tuple[str, str]: 問題の解答と作成
+            - latex_answer (str): latex形式と通常の文字列が混在していることを前提とした解答
+            - latex_problem (str): latex形式と通常の文字列が混在していることを前提とした問題
+        """
+        denominator = randint(2, 15)
+        numerator = randint(denominator, 3 * denominator)
+        latex_problem = f"\\( \\dfrac{{{numerator}}}{{{denominator}}} \\)を帯分数か整数に書きかえなさい。"
+        integer_part, numerator_part = divmod(numerator, denominator)
+        if numerator_part == 0:
+            latex_answer = f"\\( {integer_part} \\)"
+        else:
+            latex_answer = f"\\( {integer_part} \\dfrac{{{numerator_part}}}{{{denominator}}} \\)"
+        return latex_answer, latex_problem
+    
+    def _make_from_mixed_number_to_improper_fraction_problem(self):
+        """帯分数から仮分数への変換
+
+        Returns:
+            Tuple[str, str]: 問題の解答と作成
+            - latex_answer (str): latex形式と通常の文字列が混在していることを前提とした解答
+            - latex_problem (str): latex形式と通常の文字列が混在していることを前提とした問題
+        """
+        integer_part = randint(1, 3)
+        denominator = randint(2, 15)
+        numerator_part = randint(1, denominator - 1)
+        latex_problem = f"\\( {integer_part} \\dfrac{{{numerator_part}}}{{{denominator}}} \\)を仮分数に書きかえなさい。"
+        numerator = integer_part * denominator + numerator_part
+        latex_answer = f"\\( \\dfrac{{{numerator}}}{{{denominator}}} \\)"
+        return latex_answer, latex_problem
     
     def _make_addition_problem(self, with_integer_part: bool) -> Tuple[str, str]:
         """条件を満たした足し算の問題の作成
@@ -45,9 +87,45 @@ class AdditionAndSubtractionOfFraction:
             Tuple[str, str]: 問題の解答と作成
             - latex_answer (str): latex形式と通常の文字列が混在していることを前提とした解答
             - latex_problem (str): latex形式と通常の文字列が混在していることを前提とした問題
+        import sympy as sy
+        from random import randint
+
+        for _ in range(100):
+            common_denominator = randint(2, 15)
+            print(f"common_denominator: {common_denominator}")
+            # without integer part
+            answer_numerator = randint(2, common_denominator)
+            print(f"answer_numerator: {answer_numerator}")
+            numerator1 = randint(1, answer_numerator - 1)
+            print(f"numerator1: {numerator1}")
+            numerator2 = answer_numerator - numerator1
+            print(f"numerator2: {numerator2}")
+            print("------------")
         """
-        latex_answer = "dummy answer in addition problem"
-        latex_problem = "dummy problem in addition problem"
+        common_denominator = randint(2, 15)
+        if with_integer_part:
+            answer_numerator = randint(2, 2 * common_denominator)
+            numerator1 = randint(1, answer_numerator - 1)
+            numerator2 = answer_numerator - numerator1
+            latex_problem = f"次の計算をしましょう。\n \\( \\dfrac{{{numerator1}}}{{{common_denominator}}} + \\dfrac{{{numerator2}}}{{{common_denominator}}} \\)"
+            integer_part, numerator_part = divmod(answer_numerator, common_denominator)
+            if numerator_part == 0:
+                latex_answer = f"\\( {integer_part} \\)"
+            else:
+                if integer_part == 0:
+                    latex_answer = f"\\( = \\dfrac{{{answer_numerator}}}{{{common_denominator}}} \\)"
+                else:
+                    latex_answer = f"\\( = \\dfrac{{{answer_numerator}}}{{{common_denominator}}} \\left( {integer_part} \\dfrac{{{numerator_part}}}{{{common_denominator}}} \\right) \\)"
+        else:
+            answer_numerator = randint(2, common_denominator)
+            numerator1 = randint(1, answer_numerator - 1)
+            numerator2 = answer_numerator - numerator1
+            latex_problem = f"次の計算をしましょう。\n \\( \\dfrac{{{numerator1}}}{{{common_denominator}}} + \\dfrac{{{numerator2}}}{{{common_denominator}}} \\)"
+            integer_part, numerator_part = divmod(answer_numerator, common_denominator)
+            if integer_part == 0:
+                latex_answer = f"\\( = \\dfrac{{{numerator_part}}}{{{common_denominator}}} \\)"
+            else:
+                latex_answer = f"\\( = {integer_part} \\)"
         return latex_answer, latex_problem
     
     def _make_subtraction_problem(self, with_integer_part):
@@ -70,4 +148,5 @@ class AdditionAndSubtractionOfFraction:
                 - improper_fraction: 仮分数(分子が分母以下の値をとる)
                 - mixed_number: 帯分数
                 - None: 上の3つのいずれか
-        """    
+        """
+        pass

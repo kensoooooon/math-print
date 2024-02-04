2881b56f1710128649684de6189c78651601b1ab
total-study <inclusivetotalstudy@gmail.com>
2024-02-04 19:31:01 +0900

    processsing.

--- math_print/math_process/formula_with_symbol.py
+++ math_print/math_process/formula_with_symbol.py
@@ -26,7 +26,7 @@ class FormulaWithSymbol:
         selected_problem_type = choice(settings["problem_types"])
         if selected_problem_type == "expression_with_formula":
             # selected_theme = choice(["area", "volume", "weight", "price"])
-            selected_theme = "weight"
+            selected_theme = "price"
             if selected_theme == "area":
                 self.latex_answer, self.latex_problem = self._make_expression_with_formula_area_problem()
             elif selected_theme == "volume":
@@ -504,10 +504,178 @@ class FormulaWithSymbol:
                     個数の増減も同様に、1000円が2個と、x円が2個では扱いが異なる
                     一応分けておく？それとも関数内関数にする？
                         とりあえず分けておく形で。あとで結局つかなければ中に入れておく。
-                            
+            
+            問題のパターンとしては、単純な商品の加算、個数の増減と加算、割引あたりが考えられる
+            シンプルな順番だと、割引パターンが考えられる。また、個数も同じく掛け算のみで処理可能。
+            複数の商品のパタンは、単純な加算なら細かい処理はいらない（これまでの量とかと同じ）だが、割増割引や個数との兼ね合わせになると少し複雑になる
+            まずは割引、個数から実装していく
+            
+            商品名も、複数のパターンと一つだけのパターンがある。
+            いったん、候補を並べておいて、そこからsampleで必要個数だけ抜き出す？
+            
+            割引率をどのように動作させるか？ratio_problemでは、以下のように
+            ・割引率を一旦小数で与えて、そこから同時に計算まで行う関数？を用意していた
+        def _random_ratio(self, selected_ratio, digit_under_decimal_point: int = 1):   
+            # ratio_value = (10 ** -digit_under_decimal_point) * randint(1, 10 ** digit_under_decimal_point - 1)
+            if digit_under_decimal_point == 1:
+                ratio_value = 0.1 * randint(1, 9)
+            elif digit_under_decimal_point == 2:
+                ratio_value = 0.01 * randint(1, 99)
+            elif digit_under_decimal_point == 3:
+                ratio_value = 0.001 * randint(1, 999)
+            elif digit_under_decimal_point == 4:
+                ratio_value = 0.0001 * randint(1, 9999)
+            else:
+                raise ValueError(f"'digit_under_the_decimal_point' is {digit_under_decimal_point}. This must be 4 or less.")
+            ratio_value = round(ratio_value, 6)
+            if selected_ratio == "decimal":
+                ratio_out_of_latex = f"\\( {sy.latex(ratio_value)} \\)"
+            elif selected_ratio == "percentage":
+                normalized_percentage = self._decimal_normalize(sy.latex(ratio_value * 100))
+                ratio_out_of_latex = f"\\( {normalized_percentage} \\% \\)"
+            elif selected_ratio == "japanese_percentage":
+                digit_list = sy.latex(ratio_value)[2:]
+                japanese_percentage_names = ["割", "分", "厘", "毛"]
+                japanese_percentage_str = ""
+                for digit, name in zip(digit_list, japanese_percentage_names):
+                    if digit != "0":
+                        japanese_percentage_str += (digit + name)
+                ratio_out_of_latex = japanese_percentage_str
+            return ratio_value, ratio_out_of_latex
+            
+            採用するかどうかは、どこまで割合を複雑に取りたいかで変わってくる感じ
+            シンプルに◯割引だけを採用するのであれば、
+                discount_rate = sy.Integer(randint(1, 9)) * sy.Float(0.1)
+                discount_ratio_in_japanese = f"{discount_rate * 10}割"でイケる。ただ、0の表示がどうなるか
+                    0の表示がやばい
+                    そのままは使えない。関数を桁を減らして使い回す？
+                    ~割までに留めて、さらに小数の表示を省く。あるいは、割分と％で利用。いずれにしろ小数は不要
+                    小数第二位まで使う感じで？
+
+            表示の問題もある
+            decimal_normalizeをどこから引っ張る？
+        def _decimal_normalize(self, number: Union[str, sy.Integer, sy.Float]) -> str:
+            ----
+            小数点以下で末尾まで続いている連続した0を消去した文字列を返す。(eg. 100.00->100, 31.0010->31.001)
+
+            Args:
+                number (Union[str, sy.Integer, sy.Float]): 処理したい数
+
+            Returns:
+                number_str (str): 0が消去された文字列 
+            ----
+            number_str = str(number)
+            while True:
+                if ("." in number_str and number_str[-1] == "0") or (number_str[-1] == "."):
+                    number_str = number_str[:-1]
+                    continue
+                break
+            return number_str
+            
+            そこまで煩雑な処理ではないので中に入れても良い。関数として独立させても良い。
+            とりあえず独立させておいて、random_ratio経由でしか利用しないようであれば、しれっと中に入れる感じで
+            それでも主に桁絡みで何かしらの修正は必要そう
+            あとは割合の表示の方では、\\( \\)のありなしで多少の齟齬がある可能性
         """
-        latex_answer = "dummy answer."
-        latex_problem = "dummy problem."
+        def decimal_normalize(number: Union[str, sy.Integer, sy.Float]) -> str:
+            """
+            小数点以下で末尾まで続いている連続した0を消去した文字列を返す。(eg. 100.00->100, 31.0010->31.001)
+
+            Args:
+                number (Union[str, sy.Integer, sy.Float]): 処理したい数
+
+            Returns:
+                number_str (str): 0が消去された文字列 
+            """
+            number_str = str(number)
+            while True:
+                if ("." in number_str and number_str[-1] == "0") or (number_str[-1] == "."):
+                    number_str = number_str[:-1]
+                    continue
+                break
+            return number_str
+        
+        def random_ratio(selected_ratio: str, digit_under_decimal_point: int = 1) -> Tuple[sy.Float, str]:
+            """ランダムな割合と、その日本語表示をあわせて出力する
+
+            Args:
+                selected_ratio (str): 百分率(percentage)と割合(japanese_ratio)の2つから選択する
+                digit_under_decimal_point (int, optional): 小数点以下の桁数(2桁まで)を決定。デフォルトは1
+
+            Raises:
+                ValueError: digit_under_decimalに1,2以外の値が指定された場合に挙上
+                ValueError: selected_ratioにpercentage, japanese_percentage以外のものが指定された場合に挙上する
+
+            Returns:
+                Tuple[sy.Float, str]: 計算用の値と、出力用の日本語
+                - ratio_value (sy.Float())
+
+            Remind:
+                出力用の日本語は、\\( \\)で囲まないことを前提としている
+            """
+            if digit_under_decimal_point == 1:
+                ratio_value = 0.1 * sy.Integer(randint(1, 9))
+            elif digit_under_decimal_point == 2:
+                ratio_value = 0.01 * sy.Integer(randint(1, 99))
+            else:
+                raise ValueError(f"'digit_under_the_decimal_point' is {digit_under_decimal_point}. This must be 1 or 2.")
+            ratio_value = round(ratio_value, 6)
+            if selected_ratio == "percentage":
+                normalized_percentage = decimal_normalize(sy.latex(ratio_value * 100))
+                ratio_out_of_latex = f"\\( {normalized_percentage} \\% \\)"
+            elif selected_ratio == "japanese_percentage":
+                digit_list = sy.latex(ratio_value)[2:]
+                japanese_percentage_names = ["割", "分"]
+                japanese_percentage_str = ""
+                for digit, name in zip(digit_list, japanese_percentage_names):
+                    if digit != "0":
+                        japanese_percentage_str += (digit + name)
+                ratio_out_of_latex = japanese_percentage_str
+            else:
+                raise ValueError(f"")
+            return ratio_value, ratio_out_of_latex
+
+        x = sy.Symbol("x")
+        items = ["お菓子", "ジュース", "お弁当", "洗剤"]
+        # problem_theme = choice(["discount", "multiple_items_without_discount", "multiple_items_with_discount"])
+        problem_theme = "multiple_items_with_discount"
+        if problem_theme == "discount":
+            item = choice(items)
+            price_before_discount = x
+            discount_ratio_value, discount_ratio_out_of_latex = random_ratio(choice(["percentage", "japanese_percentage"]), randint(1, 2))
+            remained_ratio_value = 1 - discount_ratio_value
+            remained_ratio_str = decimal_normalize(remained_ratio_value)
+            price_after_discount = self._multiply_or_divide(price_before_discount, remained_ratio_value, multiply_or_divide="multiply")
+            latex_problem = f"初めに\\( {sy.latex(x)} \\)円の{item}がありました。"
+            latex_problem += f"{discount_ratio_out_of_latex}だけ値引きしたときの金額を、\\( x \\)を用いて表しなさい。"
+            latex_answer = f"{discount_ratio_out_of_latex}だけ値引きしたということは、"
+            latex_answer += f"値引きした後の金額を小数の割合で示すと、\\( {remained_ratio_str} \\)となる。\n"
+            latex_answer += f"よって答えは、\\( {price_after_discount} \\)円。"
+        elif problem_theme == "multiple_items_without_discount":
+            first_item, second_item = sample(items, k=2)
+            first_price, second_price = sample([x, sy.Integer(randint(1, 10) * 50)], k=2)
+            first_price_in_answer = sy.latex(first_price)
+            second_price_in_answer = sy.latex(second_price)
+            latex_problem = f"\\( {sy.latex(first_price)} \\)円の{first_item}と、"
+            latex_problem += f"\\( {sy.latex(second_price)} \\)円の{second_item}がありました。\n"
+            latex_problem += "2つを一緒に買ったときの金額を、\\( x \\)を用いて表しなさい。"
+            latex_answer = "1つ目の価格と2つ目の価格を合わせると、\n"
+            latex_answer += f"\\( {first_price_in_answer} + {second_price_in_answer} \\)円となる。"
+        elif problem_theme == "multiple_items_with_discount":
+            first_item, second_item = sample(items, k=2)
+            first_price, second_price = sample([x, sy.Integer(randint(1, 10) * 50)], k=2)
+            """
+            1つ目か2つ目、あるいは両方を値引きする
+                素直に場合分けしておいたほうが良さそう？
+                あるいは、何かしらの共通項を見出して、まとめられる分だけまとめるか。
+                あるいは、処理の方と本文の方と、いずれかに共通化を行い、残ったものを扱う
+                「◯円のアイテム1と、△円のアイテム2がありました。
+                (アイテム1/アイテム2)....」
+                処理の面倒さに噛み合っていない気もする。せいぜい3パターンなら書いたほうが見通しよくなるって感じでとりあえず。
+                もし、書いていてまとめられそうな点があったら検討する形で。
+                    item1だけ、item2だけであれば良さそう
+            """
+            discount_type = choice(["item1", ""])
         return latex_answer, latex_problem
         
     def _make_expression_with_formula_and_calculate_problem(self):
@@ -676,13 +844,27 @@ class FormulaWithSymbol:
                     数か文字しか入れないように押さえておく
             
             考えられるパターンは、
-            数×数, 数÷数←特に操作必要なし
+            数×数, 数÷数←特に操作必要なし。普通に計算
             文字×数, 数×文字, 文字÷数, 数÷文字←いずれも文字をそのままキープしておく
+            
+            もう一歩踏み込むと、1個目は数だろうと文字だろうと、通常のまま？
+                2 * x = 2 \\times x
+                2 * 300 = 600
+                保留で良い？
+                あるいは、数々のパターンの時には、さっさと計算する。いずれかが文字であれば、そのまま置いて、計算記号を挟む
         """
         # latex済想定
         if isinstance(first_amount, str):
             raise ValueError(f"'first_amount' must not be str.{first_amount} is wrong.")
+        if isinstance(second_amount, str):
+            raise ValueError(f"'first_amount' must not be str.{second_amount} is wrong.")
+        if not(isinstance(first_amount, sy.Symbol)) and not(isinstance(second_amount, sy.Symbol)):
+            calculated_amount = sy.latex(first_amount * second_amount)
         else:
-            first_value = first_amount
-        
-        
+            if multiply_or_divide == "multiply":
+                calculated_amount = f"{sy.latex(first_amount)} \\times {sy.latex(second_amount)}"
+            elif multiply_or_divide == "divide":
+                calculated_amount = f"{sy.latex(first_amount)} \\div {sy.latex(second_amount)}"
+            else:
+                raise ValueError(f"'multiply_or_divide' must be 'multiply' or 'divide'. {multiply_or_divide} is wrong.")
+        return calculated_amount

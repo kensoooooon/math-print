f8d3a322d5725819a5b6049887955cc25ab5b4a8
kensu <kensussu@gmail.com>
2024-04-10 22:15:27 +0900

    processing.

--- math_print/math_process/hs3_integration_calculation_of_linear_function_replacement.py
+++ math_print/math_process/hs3_integration_calculation_of_linear_function_replacement.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from random import choice, randint, random
 import re
-from typing import Dict, Optional, Tuple, Union
+from typing import Dict, NamedTuple, Optional, Tuple, Union
 
 import sympy as sy
 
@@ -85,7 +85,7 @@ class IntegralCalculationOfLinearFunctionReplacement:
         latex_answer, latex_problem = problem_and_answer(function_latex, differentiated_function_latex)
         return latex_answer, latex_problem
 
-    def _make_definite_substitution_of_linear_expression_problem(self, used_formula: str) -> Tuple:
+    def _make_definite_substitution_of_linear_expression_problem(self, used_formula: str) -> Tuple[str, str]:
         """1次式の置換を用いるタイプの定積分計算問題を作成
         
         Args:
@@ -104,40 +104,105 @@ class IntegralCalculationOfLinearFunctionReplacement:
                     -> それにプラスして、積分範囲を設定し、問題の情報に追加
                     あわせて、積分計算を実施する。
                     ∫^{end}_{start} f'(x) dx = [f(x)]^{end}_{start} = value.
+                
+            4/10
+            継続
+            namedtupleに必要な要素とは？というお話
+                積分元の関数, 積分先の関数, 
+                積分範囲, それらを代入したときのそれぞれの値
+                計算結果
+                
+                以上が必要そう。問題は渡し方だが、どこまでを委任するかが重要
+                -> 不定積分での利用と、役割の収まりの良さを意識すると、全部文字列が良さそう
         """
-        def problem_and_answer(function_latex_for_answer: str, function_latex_for_problem: str) -> Tuple[str, str]:
+        
+        class DefiniteIntegralInformation(NamedTuple):
+            """定積分に必要な情報を格納する
+
+            Attributes:
+                end (str): 積分範囲の終点
+                start (str): 積分範囲の始点
+                original_function (str): 積分前の関数
+                integral_of_function (str): 積分後の関数
+                end_value (str): 終点を代入したときの具体的な値
+                start_value (str): 始点を代入したときの具体的な値
+                result (str): 定積分の計算結果
+            """
+            end: str
+            start: str
+            original_function: str
+            integral_of_function: str
+            end_value: str
+            start_value: str
+            result: str
+        
+        def problem_and_answer(definite_integral_information: DefiniteIntegralInformation) -> Tuple[str, str]:
             """定積分用の問題と解答を出力
-
+            
             Args:
-                function_for_answer (str): 解答に利用したいlatex形式の式
-                function_for_problem (str): 問題に利用したいlatex形式の式
-
+                definite_integral_information (DefiniteIntegralInformation): 式を書くための情報を格納
+            
             Returns:
                 Tuple[str, str]: latex形式の問題と解答
                 - latex_answer (str): 解答
                 - latex_problem (str): 問題
             """
-            latex_answer = f"={function_latex_for_answer} + C"
-            latex_problem = f"\\int {function_latex_for_problem} \\, dx"
+            end = definite_integral_information.end
+            start = definite_integral_information.start
+            original_function = definite_integral_information.start
+            integral_of_function = definite_integral_information.integral_of_function
+            end_value = definite_integral_information.end_value
+            start_value = definite_integral_information.start_value
+            result = definite_integral_information.result
+            latex_problem = f"\\int^{{{end}}}_{{{start}}} {original_function} \, dx \n"
+            latex_answer = f"= \\left[ {integral_of_function} \\right]^{{{end}}}_{{{start}}} \n"
+            latex_answer += f"= {end_value} - {start_value} \n"
+            latex_answer += f"= {result}"
             return latex_answer, latex_problem
         
-        def problem_and_answer() -> Tuple[str, str]:
-            """定積分用の問題と解答を出力
+        function, differentiated_function = self._make_and_differentiate_function(used_formula)
+        if used_formula == "1/x":
+            pattern = r'\\left\((.*?)\\right\)'
+            function_latex = re.sub(pattern, r'| \1 |', sy.latex(function))
+        elif used_formula == "a^x":
+            pattern = r'\\log\\{\\left\((.*?)\\right\)\\}' 
+            result = re.sub(pattern, r'log\1', sy.latex(function))
+            result = re.sub(r'\\left\(|\\right\)', '', result)
+            function_latex = re.sub(r'\s+', '', result)
+        elif used_formula == "x^(1/2)":
+            latex_str = sy.latex(function)
+            sqrt_part = re.search(r"\\sqrt{[^}]+}", latex_str).group(0)
+            if "\\cdot" in latex_str:
+                other_part = re.sub(r"\\sqrt{[^}]+} \\cdot ", "", latex_str)
+            else:
+                other_part = re.sub(r"\\sqrt{[^}]+}", "", latex_str)
+            function_latex = f"{other_part} {sqrt_part}"
+        else:
+            function_latex = sy.latex(function)
+        differentiated_function_latex = sy.latex(differentiated_function)
+        # setting for definite integral
+        if used_formula in ["sin", "cos", "1/cos^2x", "1/sin^2x"]:
+            start_denominator = choice([2, 3, 4, 6])
+            start_numerator = self._random_integer(max_abs = start_denominator * 2,  positive_or_negative="positive")
+            start = sy.pi * sy.Rational(start_numerator, start_denominator)
+            end_denominator = choice([2, 3, 4, 6])
+            end_numerator = start_numerator + self._random_integer(max_abs = end_denominator * 2, positive_or_negative="positive")
+            end = sy.pi * sy.Rational(end_numerator, end_denominator)
+        else:
+            end = self._random_integer
+        end_latex = sy.latex(end)
+        start_latex = sy.latex(start)
+        original_function_latex = differentiated_function_latex
+        integral_function_latex = function_latex
+        end_value = function.subs(x, end)
+        end_value_latex = sy.latex(end_value)
+        start_value = function.subs(x, start)
+        start_value_latex = sy.latex(start_value)
+        result = end_value - start_value
+        result_latex = sy.latex(result)
+        information = DefiniteIntegralInformation(
             
-            Returns:
-                Tuple[str, str]: latex形式の問題と解答
-                - latex_answer (str): 解答
-                - latex_problem (str): 問題
-            
-            Developing:
-                あくまで、解答と問題の最終出力を行うのが基本的な働きになる
-                -> ∫^{a}_{b} f(x) = [F(x)]^{a}_{b} = F(a) - F(b) = answerまでは作りたい
-                --> 最低限、f(x), F(x), a, b, answerは受け取るだけ。計算は外部任せ
-                    というほうが、indefinite_integral側との整合性は取れる。あちらは与えられたlatexから解答を追加していただけなので、
-                --> こっちで計算も担当することは可能だが、それはそれで処理がバラけそうだし、かぶりそうだし、何よりあとで見直したとき分かりづらそう
-                
-                よって、後は渡し方。引数も悪くないが、ここではシンプルにnamedtumpleを採用するのが良さそう？
-            """
+        )
     
     def _make_and_differentiate_function(self, used_formula: str) -> Tuple:
         """与えられた積分公式に応じて、ランダムな関数の作成と微分を行う
@@ -197,7 +262,6 @@ class IntegralCalculationOfLinearFunctionReplacement:
             function = k * 2 * sy.sqrt(linear_function)
             differentiated_function = k * a * (1 / sy.sqrt(linear_function))
         elif used_formula == "x^(1/2)":
-            # next is 表示
             k = self._random_number(use_frac=True, including_zero=False)
             function = k * sy.Rational(2, 3) * linear_function * sy.sqrt(linear_function)
             differentiated_function = k * a * sy.sqrt(linear_function)

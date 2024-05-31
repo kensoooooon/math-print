a83f2c04baef17f36dba8793928e77f2ecbe2e00
kensu <kensussu@gmail.com>
2024-05-31 22:10:39 +0900

    processing.

--- math_print/math_process/area_with_linear_function.py
+++ math_print/math_process/area_with_linear_function.py
@@ -34,9 +34,22 @@
                         eナビもカワセミも、ざっと確認した限りはグラフありきの問題なので、それはなしでokそう
         
         というわけで、とりあえずlinear_functionを3つ分ここに用意することから始める
+
+5/31
+linear_function3つ分の用意から始める
+
+jsのdrawLinearFunctionが悪さをしている
+    そもそも、値が存在していない説？
+        中で確認したら、0, 0, 0, 0だった。たまたま？
+        でもなさそう。もう一つ遡って確認する
+        
+    
+    多分self.をlinear_function1, 2, 3につけ忘れていたのが原因っぽい
+    
 """
-from random import choice
-from typing import Dict
+from random import choice, randint, random
+from typing import Dict, NamedTuple
+
 
 import sympy as sy
 
@@ -49,21 +62,72 @@ class AreaWithLinearFunction:
         latex_problem (str): LaTeX形式を前提とした問題
     """
     def __init__(self, **settings: Dict):
-        
         sy.init_printing(order='grevlex')
         selected_problem_type = choice(settings["problem_types"])
         if selected_problem_type == "one_side_on_axis":
-            self.latex_answer, self.latex_problem = self._make_one_side_on_axis_problem()
+            self.latex_answer, self.latex_problem, self.linear_function1, self.linear_function2, self.linear_function3 = self._make_one_side_on_axis_problem()
         elif selected_problem_type == "no_side_on_axis":
-            self.latex_answer, self.latex_problem = self._make_no_side_on_axis_problem()
+            self.latex_answer, self.latex_problem, self.linear_function1, self.linear_function2, self.linear_function3 = self._make_no_side_on_axis_problem()
     
     def _make_one_side_on_axis_problem(self):
+        linear_function1 = self._decide_linear_function_status()
+        linear_function2 = self._decide_linear_function_status()
+        linear_function3 = self._decide_linear_function_status()
         latex_answer = "dummy answer"
         latex_problem = "dummy problem"
-        return latex_answer, latex_problem
+        return latex_answer, latex_problem, linear_function1, linear_function2, linear_function3
     
     def _make_no_side_on_axis_problem(self):
+        linear_function1 = self._decide_linear_function_status()
+        linear_function2 = self._decide_linear_function_status()
+        linear_function3 = self._decide_linear_function_status()
         latex_answer = "dummy answer"
         latex_problem = "dummy problem"
-        return latex_answer, latex_problem
+        return latex_answer, latex_problem, linear_function1, linear_function2, linear_function3
+
+    def _decide_linear_function_status(self):
+        """1次関数のステータスを決定
+
+        Returns:
+            linear_function (LinearFunction): 1次関数の式と通る点を格納
+        """
+        class LinearFunction(NamedTuple):
+            """1次関数の通る点と式を格納
+
+            Args:
+                x1, y1, x2, y2 (str): 1次関数が通る2点
+                linear_function_latex (str): latex形式で記述された1次関数の式
+                linear_coefficient_latex (str): latex形式で記述された1次関数の傾き
+                intercept_latex (str): latex形式で記述された切片
+            """
+            x1: str
+            y1: str
+            x2: str
+            y2: str
+            linear_equation_latex: str
+            linear_coefficient_latex: str
+            intercept_latex: str
+
+        x = sy.Symbol("x", real=True)
+        y = sy.Symbol("y", real=True)
+        x1 = randint(-5, 5 - 1)
+        x2 = x1 + randint(1, 5 - x1)
+        y1 = randint(-5, 5 - 1)
+        y2 = y1 + randint(1, 5 - y1)
+        if random() > 0.5:
+            y1, y2 = y2, y1
+        linear_coefficient = sy.Rational(y2 - y1, x2 - x1)
+        right_of_equation = sy.expand(linear_coefficient * x - linear_coefficient * x1 + y1)
+        right_of_equation_latex = sy.latex(right_of_equation)
+        linear_equation_latex = f"\( y = {right_of_equation_latex} \)".replace("\\", "\\\\")
+        linear_coefficient_latex = f"\( {sy.latex(linear_coefficient)} \)".replace("\\", "\\\\")
+        intercept = -linear_coefficient * x1 + y1
+        intercept_latex = f"\( {sy.latex(intercept)} \)".replace("\\", "\\\\")
+        linear_function = LinearFunction(
+            x1=str(x1), y1=str(y1),
+            x2=str(x2), y2=str(y2),
+            linear_equation_latex=linear_equation_latex,
+            linear_coefficient_latex=linear_coefficient_latex, intercept_latex=intercept_latex
+        )
+        return linear_function
     
